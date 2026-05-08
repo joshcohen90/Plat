d9f04efb04e406e1dcca7ddaf53c120ca01e1aeb
joshcohen90 <josh.cohen90@icloud.com>
2026-05-08 13:05:35 -0400

    Initial Commit

---
+++ NextStop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NextStop
+//
+//  Created by Josh on 5/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NextStop/NextStopApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextStopApp.swift
+//  NextStop
+//
+//  Created by Josh on 5/8/26.
+//
+
+import SwiftUI
+
+@main
+struct NextStopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
